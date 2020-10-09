ff8b1807d73960964497c6da78eb36bf06af34d9
Kushald2805 <coolguys280598@hotmail.com>
2020-10-09 17:20:27 +0530

    Completed Module

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,7 +9,7 @@ class Quiz extends Component{
 		const isQuizEnd = this.state.quiz_position-1 === quizData.quiz_questions.length;
 		return(
 			<div>
-			{isQuizEnd ? (<QuizEnd/>) : (<QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>)}
+			{isQuizEnd ? (<QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />) : (<QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>)}
 			</div>
 		);
 	}
@@ -21,6 +21,11 @@ class Quiz extends Component{
 	constructor(props){
 		super(props);
 		this.state={quiz_position: 1};
+	}
+
+	handleResetClick(){
+		this.setState({quiz_position:1});
+
 	}
 
 }
